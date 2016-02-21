a15052f9d98501afd1ec0f32e0f2e091f1ef9a2c
Jakub Lewek <jlewek@MacBook-Pro-Jakub.local>
2016-02-21 12:07:03 +0100

    first app stable release

--- Cross And Circle/ViewController.swift
+++ Cross And Circle/ViewController.swift
@@ -29,8 +29,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var crossOutlet: UIButton!
     
     @IBOutlet weak var resetOutlet: UIButton!
-    var game_state=gameState.Circle
-    
+    var game_state = gameState.Circle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,17 +42,7 @@ class ViewController: UIViewController {
     }
     
     func resetClicked(gesturerecorgnizer:UITapGestureRecognizer){
-        clickedArray =  [0,0,0,0,0,0,0,0,0]
-        
-        game_state = .Circle
-        
-        selectTurn()
-        
-        for sv in self.view.subviews{
-            if sv.tag>0 {
-                (sv as! UIButton).setTitle("",forState:UIControlState.Normal)
-            }
-        }
+        resetGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,9 +61,40 @@ class ViewController: UIViewController {
                     clickedArray[sender.tag-1] = 2
             }
         
+            if checkWin(clickedArray[sender.tag-1]) {
+                
+                let alert = UIAlertView()
+                alert.title = "GAME OVER"
+                
+                if clickedArray[sender.tag-1] == 1 {
+                    alert.message = "Circle is winner!"
+                } else {
+                    alert.message = "Cross is winner!"
+                }
+                
+                alert.addButtonWithTitle("OK!")
+                alert.show()
+                
+                resetGame()
+                
+            }
+            
             game_state.next()
         
             self.selectTurn()
+        }
+    }
+    
+    func resetGame(){
+        clickedArray =  [0,0,0,0,0,0,0,0,0]
+        game_state = .Circle
+        
+        selectTurn()
+        
+        for sv in self.view.subviews{
+            if sv.tag>0 {
+                (sv as! UIButton).setTitle("",forState:UIControlState.Normal)
+            }
         }
     }
     
@@ -87,6 +107,34 @@ class ViewController: UIViewController {
             circleOutlet.selected=false
             crossOutlet.selected=true
         }
+    }
+    
+    func checkWin(who: Int) -> Bool{
+        let winConfigurations = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [1,5,9],
+            [3,5,7]
+        ]
+        
+        var lineCounter = 0;
+        
+        for configuration in winConfigurations {
+            for field in configuration {
+                if clickedArray[field-1] == who {
+                    lineCounter++
+                }
+                if lineCounter == 3{
+                    return true;
+                }
+            }
+            lineCounter = 0;
+        }
+        return false;
     }
 
 }
